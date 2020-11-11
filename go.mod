3a31ad410b9f65f7afb18c1cb025cfcbf357ba33
Arut Jinadit <arut.jinadit@gmail.com>
2020-11-11 21:45:11 +0000

    [Added]: Sqlite database for result caching.

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/instrumenta/kubeval v0.0.0-20201005082916-38668c6c5b23
 	github.com/jinzhu/gorm v1.9.16
 	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
+	google.golang.org/appengine v1.1.0
 )
--- main.go
+++ main.go
@@ -3,27 +3,55 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/arut-ji/individual-project/database"
 	"github.com/arut-ji/individual-project/linter"
 	"github.com/arut-ji/individual-project/sample"
 	"github.com/arut-ji/individual-project/util"
+	"log"
 	"os"
+	"sync"
 )
 
 func main() {
 	ctx := context.Background()
 	sampler := sample.NewCodeSampler(ctx)
 	ss, err := sampler.NewSampleFromAPI(ctx, &sample.SamplingOptions{
-		Size: 2,
+		Size: 50,
 	})
 	if err != nil {
 		_ = fmt.Errorf("%v", err)
 		os.Exit(1)
 	}
+
+	kubeSamples := make(sample.Samples, 0)
 	for _, s := range *ss {
 		decodedContent, err := util.DecodeContent(s.Content)
 		if err != nil {
 			panic(err)
 		}
 		_, err = linter.Lint(decodedContent)
+		if err == nil {
+			kubeSamples = append(kubeSamples, s)
+		}
 	}
+
+	db, err := database.NewDatabase()
+	if err != nil {
+		_ = fmt.Errorf("%v", err)
+		panic(err)
+	}
+
+	var wg sync.WaitGroup
+	for _, s := range *ss {
+		s := s
+		go func() {
+			wg.Add(1)
+			log.Println("Saving: ", s)
+			err := db.Create(&s).Error
+			if err != nil {
+				log.Fatalln("Error creating a sample: ", err)
+			}
+		}()
+	}
+	wg.Wait()
 }
