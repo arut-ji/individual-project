21cfce95041ebcac0245802d7d6f4370df1c3757
Arut Jinadit <arut.jinadit@gmail.com>
2021-01-07 09:22:59 +0700

    [Added]: a test utility function - loadFixture - to help loading Kubernetes scripts while unit testing.

---
+++ linter/smells_detector/test_util.go
@@ -0,0 +1,18 @@
+package smells_detector
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+type loadFixtureFunc func(string) string
+
+func loadFixture(fixtureDir string) loadFixtureFunc {
+	return func(filename string) string {
+		content, err := ioutil.ReadFile(fmt.Sprintf("%v/%v", fixtureDir, filename))
+		if err != nil {
+			panic(err)
+		}
+		return string(content)
+	}
+}
