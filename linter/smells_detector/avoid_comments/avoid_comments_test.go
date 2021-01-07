2103c354116aa20f7221693ae419d44e7ae7d87f
Arut Jinadit <arut.jinadit@gmail.com>
2021-01-07 09:35:03 +0700

    [Refactored]: Isolated each smell detector into self-contained package.

--- linter/smells_detector/avoid_comments/avoid_comments_test.go
+++ linter/smells_detector/avoid_comments/avoid_comments_test.go
@@ -1,20 +1,23 @@
-package smells_detector
+package avoid_comments
 
-import "testing"
-
-const (
-	AvoidCommentsFixtureDir = "./fixtures/avoid_comments"
+import (
+	"github.com/arut-ji/individual-project/linter/smells_detector"
+	"testing"
 )
 
+func loadFixture(name string) string {
+	return smells_detector.LoadFixture("./fixtures")(name)
+}
+
 func TestHasCommentsForNoSmell(t *testing.T) {
-	script := loadFixture(AvoidCommentsFixtureDir)("no_smell.yaml")
+	script := loadFixture("no_smell.yaml")
 	if result, err := hasComments(script); result != false || err != nil {
 		t.Errorf("Detection result was incorrect, got: %v, want: %v.", result, false)
 	}
 }
 
 func TestHasCommentsForContainingSmell(t *testing.T) {
-	script := loadFixture(AvoidCommentsFixtureDir)("has_comments.yaml")
+	script := loadFixture("has_comments.yaml")
 	if result, err := hasComments(script); result != true || err != nil {
 		t.Errorf("Detection result was incorrect, got: %v, want: %v.", result, true)
 	}
