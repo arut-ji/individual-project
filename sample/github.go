102fa8ebb92fda28f89e3b2aec2d5c3a2fa3d2c0
Arut Jinadit <arut.jinadit@gmail.com>
2021-01-08 14:00:10 +0700

    [Added]: Comments for implementation justification in github.go. [Added]: Commit count fetching function. [Modified]: Added owner attribute in Sample struct for further  usage with Github API.

--- sample/github.go
+++ sample/github.go
@@ -43,8 +43,15 @@ func (s *sampler) NewSampleFromAPI(ctx context.Context, opts *SamplingOptions) (
 	return &samples, nil
 }
 
+// This function returns an observable emitting query results from Github API.
 func sampleFromGithub(ctx context.Context, ghc *github.Client) rxgo.Observable {
 	ch := make(chan rxgo.Item)
+	/*
+		Github API returns one single page results with a next page token.
+		The API has a known limitation: the maximum number of item returned is 1000 items.
+		In this case, we request 100 samples per page then iterate over the pages
+		until it reach the defined upper bound.
+	*/
 	perPage := 100
 	go func(ch chan rxgo.Item) {
 		for page := 0; perPage*page <= 1000; page++ {
@@ -59,24 +66,27 @@ func sampleFromGithub(ctx context.Context, ghc *github.Client) rxgo.Observable {
 					},
 				})
 			if err != nil {
+				// log.Println is used because using either "log.Fatal"  or "log.Panic" is going to panic.
 				log.Println("Error fetching codes: ", err)
-				return
+				ch <- rxgo.Error(err)
 			}
-
+			// Feed each code result into the observable stream.
 			for _, codeResult := range result.CodeResults {
 				ch <- rxgo.Of(codeResult)
 			}
+			// Impose time interval to prevent fraud detection in Github API.
 			time.Sleep(time.Second * 1)
 		}
 		close(ch)
 	}(ch)
-	return rxgo.FromEventSource(
+	return rxgo.FromChannel(
 		ch,
 		rxgo.WithContext(ctx),
 		rxgo.WithPublishStrategy(),
 	)
 }
 
+// Fetch file contents from each query result.
 func fetchContent(ctx context.Context, ghc *github.Client, owner, repo, path string) (*string, error) {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
@@ -90,6 +100,7 @@ func fetchContent(ctx context.Context, ghc *github.Client, owner, repo, path str
 	return nil, errors.New("file content not found")
 }
 
+// Map each query result to the content fetched
 func mapToContent(ghc *github.Client) rxgo.Func {
 	return func(ctx context.Context, item interface{}) (interface{}, error) {
 		result := item.(*github.CodeResult)
@@ -109,6 +120,7 @@ func mapToContent(ghc *github.Client) rxgo.Func {
 			Path:         result.GetPath(),
 			Repository:   result.GetRepository().GetFullName(),
 			RepositoryId: result.GetRepository().GetID(),
+			Owner:        repo.GetOwner().GetLogin(),
 			Fork:         result.GetRepository().GetFork(),
 			Content:      *content,
 		}, nil
@@ -132,6 +144,10 @@ func mapWithLintingResult(_ context.Context, item interface{}) (interface{}, err
 	}, nil
 }
 
-func (s *sampler) fetchCommitCounts(ctx context.Context) {
-
+func fetchCommitCount(ctx context.Context, ghc *github.Client, owner, repo string) (int, error) {
+	commits, _, err := ghc.Repositories.ListCommits(ctx, owner, repo, nil)
+	if err != nil {
+		return 1, err
+	}
+	return len(commits), nil
 }
