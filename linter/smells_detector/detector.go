dc612f66e366b08ce40743276760dc2ba5058293
Arut Jinadit <arut.jinadit@gmail.com>
2021-02-10 23:22:40 +0700

    [Fixed]: pipeline failure in every detector.

--- linter/smells_detector/detector.go
+++ linter/smells_detector/detector.go
@@ -2,9 +2,11 @@ package smells_detector
 
 import (
 	"github.com/arut-ji/individual-project/linter/smells_detector/avoid_comments"
+	"github.com/arut-ji/individual-project/linter/smells_detector/duplicated_liveness_and_readiness"
 	"github.com/arut-ji/individual-project/linter/smells_detector/improper_alignment"
 	"github.com/arut-ji/individual-project/linter/smells_detector/incomplete_tasks"
 	"github.com/arut-ji/individual-project/linter/smells_detector/long_statement"
+	"github.com/arut-ji/individual-project/linter/smells_detector/missing_readiness_probes"
 )
 
 type (
@@ -15,12 +17,12 @@ type (
 
 func newDetector() Detectors {
 	return Detectors{
-		"avoid-comments": avoid_comments.GetNumberOfInstances,
-		//"duplicate-liveness-and-readiness-probe": duplicated_liveness_and_readiness.GetNumberOfInstances,
-		"improper-alignment": improper_alignment.GetNumberOfInstances,
-		"in-complete-tasks":  incomplete_tasks.GetNumberOfInstances,
-		"long-statement":     long_statement.GetNumberOfInstances,
-		//"missing-readiness-probe":                missing_readiness_probes.GetNumberOfInstances,
+		"avoid-comments":                         avoid_comments.GetNumberOfInstances,
+		"duplicate-liveness-and-readiness-probe": duplicated_liveness_and_readiness.GetNumberOfInstances,
+		"improper-alignment":                     improper_alignment.GetNumberOfInstances,
+		"in-complete-tasks":                      incomplete_tasks.GetNumberOfInstances,
+		"long-statement":                         long_statement.GetNumberOfInstances,
+		"missing-readiness-probe":                missing_readiness_probes.GetNumberOfInstances,
 	}
 }
 
--- linter/smells_detector/missing_readiness_probes/missing_readiness_probes_test.go
+++ linter/smells_detector/missing_readiness_probes/missing_readiness_probes_test.go
@@ -28,3 +28,23 @@ func TestForOneInstance(t *testing.T) {
 		t.Errorf("Detection result was incorrect, got: %v, want: %v.", result, 1)
 	}
 }
+
+func TestForTwoInstanceOneResource(t *testing.T) {
+	script := loadFixture("two_smell_one_resource.yaml")
+	if result, err := countMissingReadinessProbes(script); result != 2 {
+		if err != nil {
+			t.Error("Detection returned errors: ", err)
+		}
+		t.Errorf("Detection result was incorrect, got: %v, want: %v.", result, 2)
+	}
+}
+
+func TestForTwoInstanceTwoResources(t *testing.T) {
+	script := loadFixture("two_smell_two_resources.yaml")
+	if result, err := countMissingReadinessProbes(script); result != 2 {
+		if err != nil {
+			t.Error("Detection returned errors: ", err)
+		}
+		t.Errorf("Detection result was incorrect, got: %v, want: %v.", result, 2)
+	}
+}
