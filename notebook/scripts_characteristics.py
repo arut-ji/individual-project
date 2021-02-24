8ef9cd470ef3cf380f7449c5d78a0ab92f15cd41
Arut Jinadit <arut.jinadit@gmail.com>
2021-02-25 00:01:45 +0700

    [Added]: Histogram for both number of resources and line count.

--- notebook/scripts_characteristics.py
+++ notebook/scripts_characteristics.py
@@ -4,23 +4,43 @@ import numpy as np
 
 
 def main():
-    detections = load_detections()
+    detections_db = load_detections()
+    detections = list(detections_db.find())
+    # Line of Codes
     loc = np.array(
         list(
             map(
                 lambda x: x['lineOfCodes'],
-                list(detections.find())
+                detections
             )
         )
     )
-    hist, bins = np.histogram(loc, bins=8)
+    # Number of Resources
+    nor = np.array(
+        list(
+            map(
+                lambda x: x['numberOfResources'],
+                detections
+            )
+        )
+    )
+    hist, bins = np.histogram(loc, bins=15)
     logbins = np.logspace(np.log10(bins[0]), np.log10(bins[-1]), len(bins))
-    (fig, ax) = plt.subplots(1, 1)
-    ax.hist(loc, bins=logbins)
-    ax.set_title(r'$\mathrm{Line-of-code \/ Histogram}$')
-    ax.set_ylabel(r"$Frequency$")
-    ax.set_xlabel(r'$log_{10}\/(LOC)$')
-    ax.set_xscale('log')
+
+    fig = plt.figure()
+
+    ax1 = fig.add_subplot(211)
+    ax1.hist(loc, bins=logbins)
+    ax1.set_ylabel(r"$Frequency$")
+    ax1.set_xscale('log')
+    # ax1.title.set_text("Line of Code")
+
+    ax2 = fig.add_subplot(212)
+    ax2.hist(nor, bins=logbins)
+    ax2.set_ylabel(r"$Frequency$")
+    ax2.set_xscale('log')
+    # ax2.title.set_text("Number of Resources")
+
     plt.show()
 
 
